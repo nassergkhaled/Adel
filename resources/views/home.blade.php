37a366013fa69b988d02f628cf2691c87d51bf61
Amr Qabaha <139641641+Qabaha4@users.noreply.github.com>
2024-03-30 06:06:44 +0300

    wallah still editing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap" rel="stylesheet">
 </head>
 
-<body class="bg-gray-100">
+<body class="bg-gray-100 font-Almarai">
 
     <nav class="bg-white shadow-md">
         <div class="max-w-7xl mx-auto">
@@ -24,32 +24,36 @@
 
                 <!-- Navigation buttons in the middle -->
                 <div class="flex-grow flex items-center justify-center">
-                    <a href="#"
+                    <a href="#main"
                         class="text-[#BF9874] px-3 py-2 rounded-md text-lg font-medium hover:text-gray-600 transition-colors duration-200">الرئيسية</a>
-                    <a href="#"
-                        class="text-[#B9B4B4] px-3 py-2 rounded-md text-lg font-medium hover:text-gray-600 transition-colors duration-200">عنا</a>
-                    <a href="#"
+                        <x-nav-link :href="'#' . $id='us'" :active="request()->is($id)">
+                            عنا
+                        </x-nav-link>
+                        
+                        
+                    <a href="#ourServices"
                         class="text-[#B9B4B4] px-3 py-2 rounded-md text-lg font-medium hover:text-gray-600 transition-colors duration-200">خدماتنا</a>
-                    <a href="#"
-                        class="text-[#B9B4B4] px-3 py-2 rounded-md text-lg font-medium hover:text-gray-600 transition-colors duration-200">شركاؤنا</a>
+                    <a href="#partners"
+                        class="text-[#B9B4B4] px-3 py-2 rounded-md text-lg font-medium hover:text-gray-600 transition-colors duration-200">شركائنا</a>
                 </div>
 
                 <!-- Buttons on the left with red background -->
                 <div class="flex items-center">
-                    <a href="{{route('login')}}" class="bg-[#BF9874] text-white px-4 py-2 rounded-md text-sm font-medium font-Almarai">تسجيل
+                    <a href="{{ route('login') }}"
+                        class="bg-[#BF9874] text-white px-4 py-2 rounded-md text-sm font-medium font-Almarai">تسجيل
                         الدخول</a>
-                    <a href="#" class="text-[#BF9874] px-3 py-2 rounded-md text-sm font-medium underline">إنشاء
+                    <a href="{{route('register')}}" class="text-[#BF9874] px-3 py-2 rounded-md text-sm font-medium underline">إنشاء
                         حساب</a>
                 </div>
             </div>
         </div>
     </nav>
 
-    <div class="relative w-auto">
+    <div class="relative w-auto" id="main">
         <img src="{{ asset('images\Rectangle 5.jpg') }}" alt="photo" class="w-screen h-auto sm:object-cover">
         <div class="absolute inset-0 bg-gradient-to-t from-black to-transparent">
             <div class="text-right mt-[310px] mr-[200px]">
-                <h1 class="text-white text-5xl font-bold my-5 w-[724px]">أصبحت برامج إدارةالممارسات القانونيـة سهلة</h1>
+                <h1 class="text-white text-5xl font-bold my-5 w-[724px] leading-relaxed">أصبحت برامج إدارةالممارسات القانونيـة سهلة</h1>
                 <p class="text-[#AAAAAA] text-lg font-bold my-5">قم بأتمتة شركتك وإانجاز المزيد من المهام في وقت أقل
                     بإستخدام ADEL</p>
                 <button
@@ -85,12 +89,35 @@
     </div>
 
     {{-- About us dev ,amr qabaha --}}
-    <div></div>
+    <div class="flex justify-between items-center space-x-reverse space-x-6 h-[636px] bg-[#EFEAE4]" id="us">
+
+        <div class="text-start w-[35%] h-[395px] ms-[10%]">
+            <h1 class="text-[#1C1C1C] text-[60px] ">عنا</h1>
+            <p class="text-[#B4B4B4] text-[16px]"> إدارة الملفات القانونية: السماح بإنشاء ملفات لكل عميل بما في ذلك
+                معلومات شخصية وخرائط شخصية وقانونية. يتم تنظيم الملفات بشكل هرمي، مع إمكانية إضافة التعليقات والوثائق
+                المتعلقة. تتبع الوقت والفواتير:يوفر النظام واجهة سهلة لتسجيل ساعات العمل والقانون القانوني لكل محامي.
+                اتخذ المحامون من إنشاء فواتير للخدمات الرائدة في الوقت والنشاط. إدارة المواعيد:يسمح النظام بجدولة
+                المواعيد والمهام للمحامين فقط. يمكن تعيين التنبيهات والتذكيرات وعدم تفويت أي موعد مهم. إدارة المستندات:
+                تتيح تخزين المستندات بشكل آمن ومنظم. يمكن مشاركة المستندات بسهولة بين أعضاء الفريق أو مع العملاء. تقارير
+                تحليلية: تقدم التقارير تقارير دورية عن أداء المكتب وتحليلات مفصلة عن النتائج والدخل. تساعد هذه التحديد
+                على تحديد المجالات التي تحتاج إلى تحديد القرار الصحيح. أمان:يوفر النظام مستويات بيانات عالية من الأمان
+                وموثوقة ولكن المعلومات القانونية. تتميز بآليات النسخ الاحتياطي والتشفير لحماية البيانات من الفقدان أو
+                كلمة المرور. باختصار، يهدف المشروع إلى توفير برامج متخصصة في إدارة مكاتب المحاماة لتقديم منصة شاملة
+                ومتكاملة وفعالة للعمل المكتبي وتسهيل والتواصل مع العملاء بطريقة مؤسسية وآمنة.</p>
+
+
+        </div>
+        <div id="ourServices"></div>
+        <div class="items-start h-full w-auto">
+            <img src="{{ asset('images/img3.png') }}" alt="" class="mt-[23px] h-[612px] w-full">
+        </div>
+
+    </div>
 
     {{-- Start of OUR FEATURES section --}}
-    <div class="bg-gray-100 w-full h-[227px]">
+    <div class="bg-gray-100 w-full h-[227px] my-2" >
         <!-- Title centered -->
-        <div class="flex justify-center items-center ">
+        <div class="flex justify-center items-center text-[#1C1C1C] ">
             <div class="text-[2.9rem] font-bold text-center font-Almarai">خدماتنـا</div>
         </div>
 
@@ -136,42 +163,7 @@
         </div>
     </div>
     {{-- End of OUR FEATURES section --}}
-<br>
-<br>
-
-<div class="flex justify-center items-center">
-    <div class="w-[1440px] h-[285px] flex flex-col justify-center items-center py-4">
-        <!-- Title centered -->
-        <div class="text-[2.9rem] font-bold text-center font-Almarai pb-3">شركاؤنا</div>
-        <!-- Partner logos -->
-        <div>
-            <img src="{{asset('images/partners.png')}}" alt="Partners Photo Here">
-        </div>
-    </div>
-
-    <div class="flex justify-between items-center space-x-reverse space-x-6 h-[636px] bg-[#EFEAE4]">
-
-        <div class="text-start w-[35%] h-[395px] ms-[10%]">
-            <h1 class="text-[#1C1C1C] text-[60px]">عنّا</h1>
-            <p class="text-[#B4B4B4] text-[16px]"> إدارة الملفات القانونية: السماح بإنشاء ملفات لكل عميل بما في ذلك
-                معلومات شخصية وخرائط شخصية وقانونية. يتم تنظيم الملفات بشكل هرمي، مع إمكانية إضافة التعليقات والوثائق
-                المتعلقة. تتبع الوقت والفواتير:يوفر النظام واجهة سهلة لتسجيل ساعات العمل والقانون القانوني لكل محامي.
-                اتخذ المحامون من إنشاء فواتير للخدمات الرائدة في الوقت والنشاط. إدارة المواعيد:يسمح النظام بجدولة
-                المواعيد والمهام للمحامين فقط. يمكن تعيين التنبيهات والتذكيرات وعدم تفويت أي موعد مهم. إدارة المستندات:
-                تتيح تخزين المستندات بشكل آمن ومنظم. يمكن مشاركة المستندات بسهولة بين أعضاء الفريق أو مع العملاء. تقارير
-                تحليلية: تقدم التقارير تقارير دورية عن أداء المكتب وتحليلات مفصلة عن النتائج والدخل. تساعد هذه التحديد
-                على تحديد المجالات التي تحتاج إلى تحديد القرار الصحيح. أمان:يوفر النظام مستويات بيانات عالية من الأمان
-                وموثوقة ولكن المعلومات القانونية. تتميز بآليات النسخ الاحتياطي والتشفير لحماية البيانات من الفقدان أو
-                كلمة المرور. باختصار، يهدف المشروع إلى توفير برامج متخصصة في إدارة مكاتب المحاماة لتقديم منصة شاملة
-                ومتكاملة وفعالة للعمل المكتبي وتسهيل والتواصل مع العملاء بطريقة مؤسسية وآمنة.</p>
-
-
-        </div>
-        <div class="items-start h-full w-auto">
-            <img src="{{ asset('images/img3.png') }}" alt="" class="mt-[23px] h-[612px] w-full">
-        </div>
-
-    </div>
+    
     <style>
         p.bubble {
             position: relative;
@@ -240,20 +232,27 @@
     </style>
 
 
-<div class="flex justify-center items-start text-center space-x-reverse space-x-6 h-[636px] bg-[#EFEAE4]">
+    <div class="flex justify-center items-start text-center space-x-reverse space-x-6 h-[636px] bg-[#EFEAE4]">
 
-    <div class="text-center mt-[3%]">
-        <br>
-        <h1 class="text-[60px] text-[#282828]">ماذا يقول عملاؤنا</h1>
-        <br>
+        <div class="text-center mt-[3%]">
+            <br>
+            <h1 class="text-[60px] text-[#282828]">ماذا يقول عملاؤنا</h1>
+            <br>
+        </div>
+
     </div>
 
 
-</div>
-
-</div>
-
-
+    <div class="flex justify-center items-center my-3" id="partners">
+        <div class="w-[1440px] h-[285px] flex flex-col justify-center items-center py-4">
+            <!-- Title centered -->
+            <div class="text-[2.9rem] font-bold text-center font-Almarai pb-3 text-[#1C1C1C]">شركاؤنا</div>
+            <!-- Partner logos -->
+            <div>
+                <img src="{{ asset('images/partners.png') }}" alt="Partners Photo Here">
+            </div>
+        </div>
+    </div>
 
 
 </body>
