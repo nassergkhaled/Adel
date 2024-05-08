8984fc31d29ced7c3badd894ca2a1b2cd18cf6b7
Amr Qabaha <139641641+Qabaha4@users.noreply.github.com>
2024-05-08 17:26:23 +0300

    update server Provider to Match with AWS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app['request']->server->set('http', true);
     }
 
     /**
