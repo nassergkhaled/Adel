9be24aa11191d739df3cb182f1e54c9f7f7cad3e
Qabaha4 <amrokabaha11@gmail.com>
2024-06-14 04:41:16 +0300

    fix connection between client and user

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -10,7 +10,7 @@ class Client extends Model
     use HasFactory;
     public function user()
     {
-        return $this->hasOne(User::class);
+        return $this->hasOne(User::class,'id','user_id');
     }
     public function lawyers()
     {
