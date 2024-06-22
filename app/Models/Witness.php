8a8ada4e65a8e0d2125b6d9224ecd6201145abfe
Qabaha4 <amrokabaha11@gmail.com>
2024-06-23 01:03:15 +0300

    edit witness id pattern using it model

--- app/Models/Witness.php
+++ app/Models/Witness.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,6 +14,37 @@ class Witness extends Model
         'ID_no',
         'contact_info',
     ];
+
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = self::generateCustomId();
+            }
+        });
+    }
+
+    private static function generateCustomId()
+    {
+        $date = Carbon::now()->format('ymd'); // Format the date as yymmdd
+        $lastRecord = self::whereDate('created_at', Carbon::today())->orderBy('created_at', 'desc')->first();
+
+        $incrementalDigit = 1;
+        if ($lastRecord) {
+            $lastId = $lastRecord->id;
+            $lastIncrementalDigit = substr($lastId, -4);
+            $incrementalDigit = (int)$lastIncrementalDigit + 1;
+        }
+
+        $formattedIncrementalDigit = str_pad($incrementalDigit, 4, '0', STR_PAD_LEFT);
+
+        return $date . $formattedIncrementalDigit;
+    }
     public function legalCases()
     {
         return $this->belongsToMany(LegalCase::class, 'case_session_witness', 'witness_id');
